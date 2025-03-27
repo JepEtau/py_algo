5700b137f65cdfb98ebb26f3dc9ce2a33ecb7f6b
Jep Etau <jep.etau@gmail.com>
2025-03-27 21:50:45 +0100

    added color_correction

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # py_algo
 Some algorithms I share
+
+
+## Color correction
+- modified from [sd-webui-stablesr colorfix](https://github.com/pkuliyi2015/sd-webui-stablesr/blob/master/srmodule/colorfix.py)
+- supports cpu, cuda(fp16/fp32)
+
+
+```python
+import numpy as np
+import torch
+from torch import Tensor
+from utils.images_io import load_image_fp32, write_image
+from utils.torch_tensor import img_to_tensor, tensor_to_img
+
+from color_correction import ColorCorrection
+
+device = "cuda"
+fp16: bool = True
+
+image_fp: str = ...
+ref_fp: str = ...
+out_fp: str = ...
+
+in_img: np.ndarray = load_image_fp32(filepath=image_fp)
+ref_img: np.ndarray = load_image_fp32(filepath=ref_fp)
+
+tensor_dtype: torch.dtype = torch.float16 if fp16 else torch.float32
+in_tensor: Tensor = img_to_tensor(
+    d_img=torch.from_numpy(in_img).to(device),
+    tensor_dtype=tensor_dtype,
+    flip_r_b=True
+)
+ref_tensor: Tensor = img_to_tensor(
+    d_img=torch.from_numpy(ref_img).to(device),
+    tensor_dtype=tensor_dtype,
+    flip_r_b=True
+)
+
+color_correction = ColorCorrection()
+color_correction = color_correction.to(device=device)
+with torch.inference_mode():
+    d_out = color_correction(in_tensor, ref_tensor)
+    d_out = torch.clamp(d_out, 0., 1.)
+
+d_out: Tensor = tensor_to_img(
+    tensor=d_out, img_dtype=torch.float32, flip_r_b=True
+)
+out_img = d_out.cpu().numpy()
+write_image(filepath=out_fp, img=out_img)
+```
+
+- Functions not provided: `load_image_fp32`, `write_image`, `img_to_tensor`, `tensor_to_img`
---
+++ color_correction.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+
+class ColorCorrection(nn.Module):
+    def __init__(
+        self,
+        levels: int = 5,
+        kernel_size: int = 3,
+    ):
+        """
+        NCHW tensors with N=1 and C=3
+        RGB orderd channels
+        """
+        super().__init__()
+        sigma: float = 1.0
+        x = torch.linspace(-(kernel_size // 2), kernel_size // 2, kernel_size)
+        gaussian = torch.exp(-x**2 / (2 * sigma**2))
+        gaussian /= gaussian.sum()
+        self.kernel: Tensor = (
+            gaussian
+            .outer(gaussian)
+            .unsqueeze(0)
+            .unsqueeze(0)
+            .repeat(3, 1, 1, 1)
+        )
+
+        self.levels: int = levels
+
+
+    @staticmethod
+    def blur(x: torch.Tensor, dilation: int, kernel: Tensor) -> Tensor:
+        pad = dilation * ((kernel.shape[2] - 1) // 2)
+        x = F.pad(x, (pad, pad, pad, pad), mode="replicate")
+        return F.conv2d(x, kernel, groups=3, dilation=dilation)
+
+
+    def forward(self, x: Tensor, ref: Tensor) -> Tensor:
+        h, w = x.shape[2:]
+        ref: Tensor = F.interpolate(
+            input=ref,
+            size=(h, w),
+            mode='bicubic',
+            align_corners=True,
+            antialias=True
+        )
+
+        kernel: Tensor = self.kernel.to(device=x.device, dtype=x.dtype)
+
+        ref_low_freq = ref
+        x_high_freq = torch.zeros_like(x)
+        for i in range(self.levels):
+            x_low_freq = self.blur(x, dilation=2**i, kernel=kernel)
+            x_high_freq += x - x_low_freq
+            x = x_low_freq
+
+            ref_low_freq = self.blur(ref_low_freq, dilation=2**i, kernel=kernel)
+
+        return x_high_freq + ref_low_freq
+
